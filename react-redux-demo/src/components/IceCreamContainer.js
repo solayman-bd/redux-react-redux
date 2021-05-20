5fb75cb04c69d8f289a316afc8cdce75e154b8be
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-05-20 14:41:19 +0600

    mod

--- react-redux-demo/src/components/IceCreamContainer.js
+++ react-redux-demo/src/components/IceCreamContainer.js
@@ -1,29 +1,26 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { buyIceCream } from '../redux'
+import React from "react";
+import { connect } from "react-redux";
+import { buyIceCream } from "../redux";
 
-function IceCreamContainer (props) {
+function IceCreamContainer(props) {
   return (
     <div>
       <h2>Number of ice creams - {props.numOfIceCreams} </h2>
       <button onClick={props.buyIceCream}>Buy Ice Cream</button>
     </div>
-  )
+  );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    numOfIceCreams: state.iceCream.numOfIceCreams
-  }
-}
+    numOfIceCreams: state.iceCream.numOfIceCreams, //as state are defined in reducer and as there are multiple reducer so state.whichReducerState.stateKey
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    buyIceCream: () => dispatch(buyIceCream())
-  }
-}
+    buyIceCream: () => dispatch(buyIceCream()),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(IceCreamContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(IceCreamContainer);
